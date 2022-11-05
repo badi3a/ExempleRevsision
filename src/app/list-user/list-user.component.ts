7b2d3641fee4a000dcfb686b4983757d76c853e8
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-11-05 11:28:55 +0100

    update

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-list-user',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent implements OnInit {
-
-  constructor() { }
+  public list: User[]
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(
+      (data:User[])=> this.list=data
+    )
   }
 
 }
